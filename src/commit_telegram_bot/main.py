0f67b0ea09db583aa5d3e388f7c3bad500b06740
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2024-08-18 18:31:56 +0000

    Update main.py

--- src/commit_telegram_bot/main.py
+++ src/commit_telegram_bot/main.py
@@ -3,12 +3,14 @@
 # Using https://www.httpdebugger.com/Tools/ViewHttpHeaders.aspx for send mess without proxy, ...
 import requests
 
+# Telegram & Bot data
 BOT_TOKEN = "6503477136:AAFSWflzZfODSk9wEW7Jk6XWoJ8AZNH2qVI"
 CHANNEL_CHAT_ID = "-1002179618133"
-MESSAGE = "Hello World!"
+MESSAGE = "HelloWorld!"
 
+# httpdebugger Data
 data = {
-    'UrlBox': 'https://api.telegram.org/bot6503477136:AAFSWflzZfODSk9wEW7Jk6XWoJ8AZNH2qVI/sendMessage?chat_id=-1002179618133&text=Hell',
+    'UrlBox': f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHANNEL_CHAT_ID}&text={MESSAGE}',
     'ContentTypeBox': '',
     'ContentDataBox': '',
     'HeadersBox': '',
@@ -22,7 +24,7 @@ data = {
 # send message to ch telegram
 def send_tel(mess, bot_token, chat_id):
     response = requests.post('https://www.httpdebugger.com/tools/ViewHttpHeaders.aspx', data=data)
-    return response
+    return response, response.text
 
 send_telegram_mess = send_tel(MESSAGE, BOT_TOKEN, CHANNEL_CHAT_ID)
 print(send_telegram_mess)
