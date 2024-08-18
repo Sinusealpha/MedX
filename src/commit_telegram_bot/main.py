fad4444f301603fe56987f39dc80479759701eaf
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2024-08-18 12:22:43 +0000

    Update main.py

--- src/commit_telegram_bot/main.py
+++ src/commit_telegram_bot/main.py
@@ -1,6 +1,10 @@
 # BDadmehr0.github commit_post_bot main.py 
 # Telgram Send message with bot api: https://api.telegram.org/bot<BOT_TOKEN>/sendMessage?chat_id=<CHAT_ID>&text=<TEXT>
 
+BOT_TOKEN = "your_bot_token_here"
+CHANNEL_CHAT_ID = "@your_channel_username"
+MESSAGE = "Hello World!"
+
 def send_tel(mess, bot_token, chat_id):
     return True
 
