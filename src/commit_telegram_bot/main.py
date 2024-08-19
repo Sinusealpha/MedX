b5b72ca62dfa5e5f547f24b3abc9e444532e9efe
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2024-08-19 11:37:52 +0000

    Update main.py

--- src/commit_telegram_bot/main.py
+++ src/commit_telegram_bot/main.py
@@ -1,51 +1,29 @@
+# last commit data API: https://api.github.com/repos/Sinusealpha/MedX/branches/master
 import requests
-from bs4 import BeautifulSoup
 
 BOT_TOKEN = "6503477136:AAFSWflzZfODSk9wEW7Jk6XWoJ8AZNH2qVI"
 CHANNEL_CHAT_ID = "-1002179618133"
 MESSAGE = "HelloWorld!"
 
-# httpdebugger Data
-data = {
-    'UrlBox': f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHANNEL_CHAT_ID}&text={MESSAGE}',
-    'ContentTypeBox': '',
-    'ContentDataBox': '',
-    'HeadersBox': '',
-    'RefererBox': '',
-    'AgentList': 'Internet Explorer',
-    'AgentBox': '',
-    'VersionsList': 'HTTP/1.1',
-    'MethodList': 'GET',
-}
 
-# URL for the GitHub commits page
-GITHUB_URL = "https://github.com/Sinusealpha/MedX/commits/main/"
-
-def ex_data_github():
-    # Fetch the content from GitHub commits page
-    response = requests.get(GITHUB_URL)
-    response.raise_for_status()  # Check for request errors
-
-    # Parse the HTML content
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Example: Extract all commit links (modify as needed)
-    commit_links = soup.find_all('a', class_='commit-tease-sha')
-    extracted_data = [link.get('href') for link in commit_links]
-
-    # Log or print extracted data (for debugging)
-    print("Extracted Data:", extracted_data)
-    
-    # Optionally, send extracted data via Telegram
-    if extracted_data:
-        message = f"Extracted Commit Links:\n" + "\n".join(extracted_data)
-        send_tel(message, BOT_TOKEN, CHANNEL_CHAT_ID)
-    
-    return extracted_data
 
 def send_tel(mess, bot_token, chat_id):
+    # httpdebugger Data
+    data = {
+        'UrlBox': f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={mess}',
+        'ContentTypeBox': '',
+        'ContentDataBox': '',
+        'HeadersBox': '',
+        'RefererBox': '',
+        'AgentList': 'Internet Explorer',
+        'AgentBox': '',
+        'VersionsList': 'HTTP/1.1',
+        'MethodList': 'GET',
+    }
+
     response = requests.post('https://www.httpdebugger.com/tools/ViewHttpHeaders.aspx', data=data)
     return response
 
-send_telegram_mess = send_tel(MESSAGE, BOT_TOKEN, CHANNEL_CHAT_ID)
-print(send_telegram_mess)
+
+# send_telegram_mess = send_tel(MESSAGE, BOT_TOKEN, CHANNEL_CHAT_ID)
+# print(send_telegram_mess, extracted_data)
